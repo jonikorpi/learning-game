7a221812906f9b27042185e7de52a42e1f4a3e7a
Joni Korpi <me@jonikorpi.com>
2016-02-04 22:43:35 +0200

    Adds segment spokes, better tile positioning

--- client/tiles.js
+++ client/tiles.js
@@ -1,9 +1,8 @@
-Game.tilesPerRow = 9;
-Game.tilesPerColumn = 62;
-Game.tileSize = 1000;
+Game.tileSize = 1;
 
-Game.tileDepthModifier = 3;
-Game.tileHeightModifier = 0.236;
+Game.rotationOffset = (Math.PI * 0.5);
+Game.tileHeightModifier = 3;
+Game.segmentHeightModifier = 1.236;
 
 Game.tileDistance = (Game.tilesPerColumn * Game.tileSize) / (2 * Math.PI);
 Game.tileCount = Game.tilesPerRow * Game.tilesPerColumn;
@@ -14,14 +13,14 @@ Template.game.helpers({
   tiles: function() {
     let column = 0;
     let row = 0;
-    var tiles = [];
+    let tiles = [];
 
     while (row < Game.tilesPerColumn) {
       while (column < Game.tilesPerRow) {
         tiles.push(
           {
             loc: [column, row],
-            type: _.random(0, 1)
+            type: _.random(0, 3)
           }
         );
 
@@ -33,29 +32,56 @@ Template.game.helpers({
     }
 
     return tiles;
+  },
+
+  segments: function() {
+    let segment = 0
+    let segments = [];
+
+    while (segment < Game.segments) {
+      segments.push(
+        {
+          loc: segment
+        }
+      );
+
+      segment++;
+    }
+
+    return segments;
   }
 });
 
 Template.tile.helpers({
   attributes: function() {
     let color = "white";
-    let zTranslate = 0;
     let metalness = 0;
     let roughness = 0.146;
+    let height = Game.tileSize * Game.tileHeightModifier;
+    let yTranslate = Game.tileSize * Game.tileHeightModifier * 0.5;
+    let depth = Game.tileSize;
 
     switch (this.type) {
       case 0:
+      case 3:
         color = "#77C3F2";
+        depth = depth * 1.236;
         break;
       case 1:
         color = "#4F7302";
-        zTranslate = -Game.tileSize * Game.tileHeightModifier;
         metalness = 0.236;
         roughness = 1.0;
+        yTranslate = yTranslate * 0.764
+        break;
+      case 2:
+        color = "rgb(60, 37, 23)";
+        metalness = 0.0;
+        roughness = 1.0;
+        yTranslate = yTranslate * 0.764
         break;
     }
 
-    const rad = this.loc[1] * (2 * Math.PI) / Game.tilesPerColumn;
+    const rad = this.loc[1] * (2 * Math.PI) / Game.tilesPerColumn - Game.rotationOffset;
 
     const x = (this.loc[0] * Game.tileSize) + (Game.tilesPerRow * Game.tileSize * -0.5) + (Game.tileSize * 0.5);
     const y = Game.tileDistance * Math.cos(rad);
@@ -64,7 +90,7 @@ Template.tile.helpers({
     const angle = Game.rightAngle(z, y) * -1 + 90;
 
     const position = `${x} ${y} ${z}`;
-    const translate = `0 ${zTranslate} 0`;
+    const translate = `0 ${yTranslate} 0`;
     const rotation = `${angle} 0 0`;
 
     return {
@@ -73,9 +99,9 @@ Template.tile.helpers({
       geometry: `
         primitive : box;
         translate : ${translate};
-        height    : ${Game.tileSize * Game.tileDepthModifier};
+        height    : ${height};
         width     : ${Game.tileSize};
-        depth     : ${Game.tileSize};
+        depth     : ${depth};
       `,
       material: `
         metalness : ${metalness};
@@ -94,6 +120,47 @@ Template.tile.helpers({
       direction: "alternate",
       repeat: "indefinite",
       easing: "ease-sine",
+    };
+  }
+});
+
+Template.segment.helpers({
+  attributes: function() {
+    let color = "white";
+    let metalness = 0.236;
+    let roughness = 0.618;
+    let yTranslate = Game.tileSize * Game.tileHeightModifier * Game.segmentHeightModifier * 0.5 * 0.618;
+
+    const rad = this.loc * (2 * Math.PI) / Game.segments - Game.rotationOffset;
+
+    const x = 0;
+    const y = Game.tileDistance * Math.cos(rad);
+    const z = Game.tileDistance * Math.sin(rad);
+
+    const angle = Game.rightAngle(z, y) * -1 + 90;
+
+    const position = `${x} ${y} ${z}`;
+    const translate = `0 ${yTranslate} ${Game.tileSize * 0.5}`;
+    const rotation = `${angle} 0 0`;
+
+    if (this.loc == 0)
+      color = "red"
+
+    return {
+      position: position,
+      rotation: rotation,
+      geometry: `
+        primitive : box;
+        translate : ${translate};
+        height    : ${Game.tileSize * Game.tileHeightModifier * Game.segmentHeightModifier};
+        width     : ${Game.tileSize * Game.tilesPerRow};
+        depth     : ${Game.tileSize / 20};
+      `,
+      material: `
+        metalness : ${metalness};
+        color     : ${color};
+        roughness : ${roughness};
+      `,
     };
   }
 });
